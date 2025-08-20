da62a6681ffe4a542f99f58c0036423e5f1e8d36
a1phaki <jok.jok.875@gmail.com>
2025-08-20 20:55:03 +0800

    gh-pages環境指令設置

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ProfileView from '../views/ProfileView.vue'
 import WorkView from '../views/WorkView.vue'
@@ -6,7 +6,7 @@ import LineLiffView from '../views/LineLiffView.vue'
 import CharingStationView from '@/views/ChargingStationView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
