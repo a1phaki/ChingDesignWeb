f805ad9e3dec6fa77d8ebdf5aafc611231bdb297
a1phaki <jok.jok.875@gmail.com>
2025-10-16 23:27:18 +0800

    修改網頁icon 名稱

--- src/main.js
+++ src/main.js
@@ -4,9 +4,13 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faCuttlefish } from '@fortawesome/free-brands-svg-icons'
 
+library.add(faCuttlefish)
 const app = createApp(App)
 
 app.use(router)
-
+app.component('FontAwesomeIcon', FontAwesomeIcon)
 app.mount('#app')
